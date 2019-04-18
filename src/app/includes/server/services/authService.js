081b75119b10e14133f033b7274223b6133a6050
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2019-04-18 17:29:05 -0400

    comment out getUsers

--- src/app/includes/server/services/authService.js
+++ src/app/includes/server/services/authService.js
@@ -11,33 +11,33 @@ class authService {
         this.req = req;
         this.res = res;
     }
-    getUsername() {
-        let self = this;
-        try {
-            MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-                var db = client.db('ClimbingClubDB')
-                assert.equal(null, err);
-                let eboard = [];
-                let cursor = db.collection('eboard').find();
-                cursor.each((err, doc) => {
-                    assert.equal(err, null);
-                    if (doc != null) { eboard.push(doc) }
-                    else {
-                        return self.res.status(200).json({
-                            status: "success",
-                            data: eboard
-                        });
-                    }
-                });
-            });
-        }
-        catch (error) {
-            return self.res.status(500).json({
-                status: "error",
-                error: error
-            });
-        }
-    }
+    // getUsername() {
+    //     let self = this;
+    //     try {
+    //         MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    //             var db = client.db('ClimbingClubDB')
+    //             assert.equal(null, err);
+    //             let eboard = [];
+    //             let cursor = db.collection('eboard').find();
+    //             cursor.each((err, doc) => {
+    //                 assert.equal(err, null);
+    //                 if (doc != null) { eboard.push(doc) }
+    //                 else {
+    //                     return self.res.status(200).json({
+    //                         status: "success",
+    //                         data: eboard
+    //                     });
+    //                 }
+    //             });
+    //         });
+    //     }
+    //     catch (error) {
+    //         return self.res.status(500).json({
+    //             status: "error",
+    //             error: error
+    //         });
+    //     }
+    // }
 
     addUsername() {
         let self = this;
