ec28faa31b37a1447e6bc7f7d58a0175b0552ede
Gorman98 <31937140+Gorman98@users.noreply.github.com>
2019-05-02 20:42:30 -0400

    log out and log in done

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+//import { JwtHelperService } from '@auth0/angular-jwt';
+import { UtilityService } from '../utility.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  images = [1].map((currElement,index) => 'src/app/includes/images/logo.png');
+  @Input() logged:boolean;
+  images = [1].map((currElement, index) => 'src/app/includes/images/logo.png');
 
+  constructor(public utilityService: UtilityService) { 
+  }
   
-  constructor() { }
-
   ngOnInit() {
+  }
+
+  public logout() {
+    sessionStorage.removeItem('SESSIONID');
+    this.utilityService.updateLoggedIn();
   }
 
 }
